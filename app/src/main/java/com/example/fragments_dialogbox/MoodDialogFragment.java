4db0f90341d74902230aeb540499c4e9096b236b
name <collins.odinga@student.moringaschool.com>
2022-05-26 11:10:06 +0300

    added files

---
+++ app/src/main/java/com/example/fragments_dialogbox/MoodDialogFragment.java
@@ -0,0 +1,17 @@
+package com.example.fragments_dialogbox;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.DialogFragment;
+
+public class MoodDialogFragment  extends DialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
+        getDialog().setTitle("Simple Dialog");
+        return rootView;
+    }
+}
